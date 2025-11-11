250973f205439e73b5efd38f74976203a05f05c2
powermate <218421152+powermate25@users.noreply.github.com>
2025-11-11 06:15:12 +0000

    Checkpoint save: Tic Tac Toe project progress. - UI logic progress. - Game UI logic started
    * Lot of draft code in the project in case I
    need to rewind, sorry! Might clean up a bit once
    logic fully done.

--- script.js
+++ script.js
@@ -2,29 +2,42 @@ console.log("Welcome!")
 clog = console.log
 clog("clog now works!")
 
-//////////// Public variables
+//////////// Public variables 
+let player1moves = []
+let player2moves = []
+const case1 = document.querySelector("c1")
+game = (function(){
+    //// Game UI Display start
 
+const testBoard = ["Ignore-0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+
+
+
+
+//// Game UI Display end  
+    
 /* let player1moves = []
 let player2moves = [] */
 
-//player1moves.push("1")
-//player2moves.push("9")
-// player1moves.push("6")
-//player2moves.push("4") 
-//player1moves.push("8")
-// player2moves.push("5")
-//player1moves.push("3")
-// player2moves.push("7")
-//player1moves.push("2")
+/* player1moves.push("1")
+player2moves.push("9")
+player1moves.push("6")
+player2moves.push("4") 
+player1moves.push("8")
+player2moves.push("5")
+player1moves.push("3")
+player2moves.push("7")
+player1moves.push("2") */
 
 //player1moves.push("3")
 //player1moves.push("9")
-// player1moves.push("4")
+//player1moves.push("4")
 // player1moves.push("6")
 
 
 //////////// Private 
-const ticTacToe = (function(){
+const ticTacToe = function(){
+   
 // Everything inside here's private
 // Declaring dependencies
 function makePlayer(name, marker){
@@ -34,8 +47,8 @@ function makePlayer(name, marker){
 
 const player1 = makePlayer("Bob", "X")
 const player2 = makePlayer("Diana", "O")
-let player1moves = []
-let player2moves = []
+/* let player1moves = []
+let player2moves = [] */
 let cpuMoves = player2moves
 let validCpuMove
 let player1WinsCondition
@@ -45,8 +58,8 @@ const gameBoard = ["Ignore-0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
 
 
 /// test
-player1moves.push("3")
-player1moves.push("9")
+// player1moves.push("3")
+// player1moves.push("9")
 //test
 
 //fn start
@@ -74,7 +87,7 @@ winningSet()
 
  
     /// IIFE start
-const getCurrentCpuMove = (function(){
+const getCurrentCpuMove = function(){
     let currentCpuMove
     getCpuMove = function(){
     tempCheck = Number((Math.random() * 10 ).toFixed(0))
@@ -100,11 +113,16 @@ for (i = 1; i <= 1000; i++){
     tempArray.push(`${tempNumber}`)
 clog("// " + player1moves + " lenght: " + player1moves.length)
 clog( (player1moves.filter( y => !tempArray.includes(y)).length === player1moves.length) )  
- 
-if ( (player1moves.filter( y => tempArray.includes(y) === false).length === player1moves.length) === false ) {
+
+let badCpuMove =  (player1moves.filter( y => tempArray.includes(y) === false).length === player1moves.length) === false 
+let badCpuMoveSelfOccupied = (player2moves.filter( y => tempArray.includes(y) === false).length === player2moves.length) === false 
+let goodCpuMove = (player1moves.filter( y => tempArray.includes(y) === false).length === player1moves.length) === true 
+let goodCpuMoveNonSelfOccupied = (player2moves.filter( y => tempArray.includes(y) === false).length === player2moves.length) === true
+
+if ( badCpuMove || badCpuMoveSelfOccupied ) {
     clog("No good number: " + tempArray)
     continue}
-else if ( (player1moves.filter( y => tempArray.includes(y) === false).length === player1moves.length) === true ) {
+else if ( goodCpuMove && goodCpuMoveNonSelfOccupied ) {   
     clog("Great number:" + tempArray)
     validCpuMove = tempNumber 
     clog("final valid cpu number is: " + validCpuMove)
@@ -113,30 +131,40 @@ else if ( (player1moves.filter( y => tempArray.includes(y) === false).length ===
     clog("*******")
 //let tempCounter = [`${validCpuMove}`]
 let realCounterMove
+let cpuSelfWinMove
 for(set in winnerSet){
     cpuCounter = winnerSet[set].filter(z => !player1moves.includes(z) === true )
+    cpuSelfWin = winnerSet[set].filter(o => player2moves.includes(o) === true )
     clog(
-    cpuCounter = winnerSet[set].filter(z => !player1moves.includes(z) === true ),
- // cpuCounterFiltered = cpuCounter.filter(q => tempCounter.includes(q) === false )  
+    cpuCounter, 
+   "sw is this: " + cpuSelfWin   
+  
     )
+ 
+if (cpuSelfWin.length === 1 && cpuSelfWin !== undefined ) {
+    cpuSelfWinMove = cpuSelfWin 
+    clog("sw is: " + cpuSelfWinMove) 
+    validCpuMove = Number(cpuSelfWinMove.join())
+    clog(validCpuMove)  }
 
- if (cpuCounter.length === 1 && cpuCounter !== undefined ) {
-    realCounterMove = cpuCounter
+ else if (cpuCounter.length === 1 && cpuCounter !== undefined ) { 
+    realCounterMove = cpuCounter 
     clog("rc is: " + realCounterMove)
     validCpuMove = Number(realCounterMove.join())
     clog(validCpuMove)  }
  }
     clog( "******")
-} 
+}  
 
     break}
 
 }               
 
+
 return validCpuMove         
-})()
+}
 /// IIFE end  
-           
+ 
 clog("player moves below: ") 
 clog("P1:")     
 clog(player1moves) 
@@ -149,12 +177,12 @@ clog(cpuMoves)
 /// IIFE fn start
 let currentBoardState
 
-const updateBoard = (() => {
+const updateBoard = () => {
     let tempBoard = gameBoard.filter(i => !player1moves.includes(i)) 
     let updatedBoard = tempBoard.filter(i => !player2moves.includes(i))
     currentBoardState = updatedBoard
     return (currentBoardState) 
-    })()
+    }
 /// IIFE fn end
 
 clog(currentBoardState)
@@ -163,7 +191,7 @@ clog(currentBoardState)
 
 /// checking for winner
 /// IIFE fn start
-checkForWinner = (function(){
+checkForWinner = function(){
     for (i in winnerSet){
         player1WinsCondition = winnerSet[i].filter(x => player1moves.includes(x) )
         player2WinsCondition = winnerSet[i].filter(x => player2moves.includes(x) )
@@ -187,11 +215,37 @@ checkForWinner = (function(){
     
    // clog(winnerSet[i])
 }
-})() 
+}
 /// IIFE fn end
 
-
-
+ 
+const gameBoardUI = document.querySelector(".gameboard-container")
+gameBoardUI.addEventListener(
+    "click", (e) => {
+        e.preventDefault
+        clog("Target clicked id is: " +  e.target.id)
+        player1moves.push(`${e.target.id.slice(1)}`)
+        clog(player1moves)
+       e.target.classList.add("occupied")
+       e.target.style.cssText = " background-color: yellow; "
+      // e.target.append(userMarker)
+       clog(e.target.classList) 
+       winningSet ()
+      let p2move = getCurrentCpuMove()
+        player2moves.push(p2move)
+        cpuOccupiedCase = document.querySelector(`#c${validCpuMove}`)     
+        cpuOccupiedCase.style.cssText = "background-color: blue"  
+        updateBoard()
+        clog(player1moves)
+        clog(player2moves)
+        clog(currentBoardState) 
+        checkForWinner()      
+       
+       
+       
+      
+    }
+) 
 
 
 
@@ -208,22 +262,22 @@ clog(
 if (finalWinner) {return "This Game Winner Is " + finalWinner.name}
 if (!finalWinner){return "Game ongoing ..."}
 // ("Returns nothing yet!")
+
+
+
+
+
+
 }
-)()
- 
-
-
-//// Game UI Display
-
-
-
-
-
-
-
 
 
 
 //////////////////////////////////////////
-clog("IIFE <return> Logger: " + ticTacToe)
+clog("IIFE <return> Logger: " + ticTacToe())
 
+
+
+
+
+
+})()
